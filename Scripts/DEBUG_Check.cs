bbcdacbbd085971a3b9da5de9ad5ab2a0db659bc
an-alch3mist <kaeya000@gmail.com>
2025-07-24 01:40:07 +0600

    day-0

--- Scripts/DEBUG_Check.cs
+++ Scripts/DEBUG_Check.cs
@@ -17,8 +17,6 @@ namespace SPACE_RPG2D
 			}
 		}
 
-		[SerializeField] string Query = "";
-		[SerializeField] GameObject Obj_ansc;
 		IEnumerator STIMULATE()
 		{
 			#region frame_rate
@@ -26,6 +24,13 @@ namespace SPACE_RPG2D
 			yield return null;
 			#endregion
 
+			//this.Check_map_refine();
+			//this.Check_gameObjectQuery();
+		}
+
+
+		void Check_map_refine()
+		{
 			List<int> LIST = new List<int>()
 			{
 				0, 1, 4, 10, 100, 1000, -1, 0, 1, 2,
@@ -34,8 +39,14 @@ namespace SPACE_RPG2D
 			LOG.SaveLog(LIST.map(elem => elem).ToTable(name: "LIST<int> original"));
 			LOG.SaveLog(LIST.map(elem => elem * Mathf.PI).ToTable(name: "LIST<float> from map"));
 			LOG.SaveLog(LIST.refine((elem, i) => (i % 3 == 0)).map((elem, i) => elem ).ToTable(name: "LIST<float> from refine + map"));
+		}
 
+		[SerializeField] string Query = "";
+		[SerializeField] GameObject Obj_ansc;
+		void Check_gameObjectQuery()
+		{
 			Debug.Log(this.Obj_ansc.Query(this.Query));
 		}
+
 	} 
 }
